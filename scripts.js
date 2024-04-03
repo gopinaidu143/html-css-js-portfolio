1cb0ba159de345ee0cf5f76fef02a922bf4f7a9a
gopinaidu143 <gopinaiduvarikuti143@gmail.com>
2024-04-03 16:40:38 +0530

    Delete scripts.js

--- scripts.js
+++
@@ -1,6 +0,0 @@
-function toggleMenu(){
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamberger-icon");
-    menu.classList.toggle('open')
-    icon.classList.toggle('open')
-}
